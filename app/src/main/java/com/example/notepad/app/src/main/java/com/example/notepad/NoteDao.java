acbeeacc045a61a7af53da93bcbedf7552211aaa
ajisiti <gadissiti77@gmail.com>
2025-10-05 02:54:13 +0700

    Create NoteDao.java

---
+++ app/src/main/java/com/example/notepad/app/src/main/java/com/example/notepad/NoteDao.java
@@ -0,0 +1,20 @@
+package com.example.notepad;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
+    List<Note> getAllNotes();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertNote(Note note);
+
+    @Delete
+    void deleteNote(Note note);
+}
